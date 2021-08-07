dd50e3417bb8337418f57ed1ecf17cf34670f3e6
Momen-Code <59899828+Momen-Code@users.noreply.github.com>
2021-08-07 18:28:11 +0200

    finished the student lectures screen in his dashboard

--- src/student-dashboard/routes/MyLectures/index.tsx
+++ src/student-dashboard/routes/MyLectures/index.tsx
@@ -4,12 +4,15 @@ import React from "react";
 import styles from "./index.module.scss";
 
 //Components
-import { Header } from "student-dashboard/components";
+import { Header, Footer } from "student-dashboard/components";
+import { LecturesSlider } from "./components";
 
 const MyLectures = () => {
   return (
     <div className={styles.myLecturesContainer}>
       <Header />
+      <LecturesSlider />
+      <Footer />
     </div>
   );
 };
